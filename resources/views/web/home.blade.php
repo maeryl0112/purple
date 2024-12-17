ccf9c6c17a401f9b7efbf6dee8ff0198f4600683
maeryl0112 <gocongmarina@gmail.com>
2024-12-18 01:15:08 +0800

    Update home.blade.php

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -246,7 +246,7 @@ data-aos="fade-up"
                         <div class="border-t border-darkPurple px-6 py-4">
                             <h3 class="text-lg font-medium text-darkPurple">Contact</h3>
                             <p class="mt-1 text-gray-600">Email: purplelookhairsalonandspa@gmail.com</p>
-                            <p class="mt-1 text-gray-600">Phone: +63 910 7171 482</p>
+                            <p class="mt-1 text-gray-600">Phone: +63 962 5351 420</p>
                         </div>
                     </div>
                 </div>
@@ -301,7 +301,7 @@ data-aos="fade-up"
                         d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
                         clip-rule="evenodd" />
                     </svg>
-                    011 554 5521
+                    +63 962 5351 420
                 </p>
             </div>
 
